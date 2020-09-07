5e3a8bc324f72e3d2a8ed9a7882ed3d849f756c6
Mariusz Żukowski <zukowski.mariusz@gmail.com>
2020-09-07 21:43:02 +0200

    Add project files.

---
+++ MZ.Common.Tests/Extensions/ListExtension.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MZ.Common.Tests
+{
+    public class ListExtension
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MZ.Common/Extensions/ListExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MZ.Common.Extensions
+{
+    public static class ListExtension
+    {
+        public static IList<IList<string>> SplitList(this IList obj, int count = 5)
+        {
+            return null;
+        }
+    }
+}
